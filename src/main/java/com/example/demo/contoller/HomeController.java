6341508f7b030fb02436c7e941641829d36efe5c
Loge <kyw9509@gmail.com>
2021-11-24 02:12:07 +0900

    홈 컨트롤러 반환 값 변경

--- src/main/java/com/example/demo/contoller/HomeController.java
+++ src/main/java/com/example/demo/contoller/HomeController.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 @RequestMapping("/")
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "github actions test home";
+        return "Github Actions Test Home";
     }
 }
